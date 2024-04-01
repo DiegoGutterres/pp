eb7e2e6e9468781b7cc6395e77d695ae765a385c
DIEGO GUTERRES DE FIGUEIREDO <02954085029@senacrs.edu.br>
2024-04-01 09:02:50 -0300

    dabase

---
+++ db_tlinth.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE db_tlinth;
+USE db_tlinth;
+
+CREATE TABLE req_user(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    modes VARCHAR(12),
+    quantity_req INT
+);
+
+CREATE TABLE res_user(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	user_id INT, 
+    response VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES req_user(id)
+);
+
+insert into req_user (modes, quantity_req) 
+values('qr ca nd', 3);
+
+select * from req_user;
+select * from res_user;
+
+insert into res_user(response, user_id)
+values('-dfkiaospfkaf', 1);
