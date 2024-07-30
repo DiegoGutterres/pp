9067526a5a658964b6c158faa3aaf12814843f63
DIEGO GUTERRES DE FIGUEIREDO <02954085029@senacrs.edu.br>
2024-07-30 09:07:56 -0300

    banco de dados

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, render_template, redirect, url_for
+import pymysql  
 import os
 from main import process_image_and_generate_response, process_pdf_and_generate_response
 
 app = Flask(__name__, static_folder='frontend/static', static_url_path='/static')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'db_tlinth'
+
+# Conexão com o banco de dados
+conn = pymysql.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    db=app.config['MYSQL_DB'],
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def storeRes(modes, response):
+    with conn.cursor() as cursor:
+        sql = "INSERT INTO res_user (modes, response) VALUES (%s, %s)"
+        cursor.execute(sql, (modes, response)) 
+    conn.commit()
 
 
 UPLOAD_FOLDER = os.path.join('frontend', 'uploads')
@@ -29,15 +51,23 @@ def upload_file():
 
 
         if '.png' in file.filename or '.jpg' in file.filename:
+
             img_text, simplified_text = process_image_and_generate_response(filepath)
             response_data['img_text'] = img_text
             response_data['simplified_text'] = simplified_text
+
+            storeRes('ARQUIVO', simplified_text)
             return jsonify({'success': True}), 200
+        
         elif '.pdf' in file.filename:
+        
             pdf_text, simplified_text = process_pdf_and_generate_response(filepath)
             response_data['pdf_text'] = pdf_text
             response_data['simplified_text'] = simplified_text
+
+            storeRes('ARQUIVO', simplified_text)
             return jsonify({'success': True}), 200
+        
     except Exception as e:
         print(f"Error processing file: {e}")
         return jsonify({'error': str(e)}), 500
@@ -66,4 +96,4 @@ def send_static(path):
     return send_from_directory('frontend/static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='172.20.90.4')
+    app.run(debug=True)
---
+++ frontend/uploads/main.py
@@ -0,0 +1,55 @@
+from pytesseract import pytesseract
+import google.generativeai as genai
+from pdfquery import PDFQuery
+import os
+
+# api init
+genai.configure(api_key="AIzaSyCiORc74qB0QGtY0ZgZ_Z9Xw1j2aWHceNA")
+model = genai.GenerativeModel('models/gemini-1.5-flash')
+
+# Configuração do pytesseract
+path_to_tes = r"C:\Users\DIEGOGUTERRESDEFIGUE\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract.tesseract_cmd = path_to_tes
+
+def process_image_and_generate_response(img_path):
+    try:
+        img_text = pytesseract.image_to_string(img_path)
+        prompt = f"""
+        Você é um advogado. Traduza todos os documentos e informações que você receber para que qualquer pessoa entenda. 
+        Simplifique o MÁXIMO que conseguir, use palavras simples que todos conheçam. Faça apenas um parágrafo (se possível),
+        que contenha todo o conteúdo importante do documento, como a decisão do juiz, o caso em si etc.
+
+        Aqui vai o texto para ser simplificado: {img_text}
+        """
+
+        response_bruto = model.generate_content(contents=prompt)
+        response = response_bruto.text
+        return img_text, response
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        raise e
+    
+def process_pdf_and_generate_response(pdf):
+    try:
+        pdf = PDFQuery('example.pdf')
+        pdf.load()
+
+        text_elements = pdf.pq('LTTextLineHorizontal')
+        pdf_text = [t.text for t in text_elements]
+
+        prompt = f"""
+        Você é um advogado. Traduza todos os documentos e informações que você receber para que qualquer pessoa entenda. 
+        Simplifique o MÁXIMO que conseguir, use palavras simples que todos conheçam. Faça apenas um parágrafo (se possível),
+        que contenha todo o conteúdo importante do documento, como a decisão do juiz, o caso em si etc.
+
+        Aqui vai o texto para ser simplificado: {pdf_text}
+        """
+
+        response_bruto = model.generate_content(contents=prompt)
+        response = response_bruto.text
+        return pdf_text, response
+    except Exception as e:
+        print(f"Error processing pdf: {e}")
+        raise e
+
+
