68e3a32836097555d11b7f72df6a7b2ac496185d
DIEGO GUTERRES DE FIGUEIREDO <02954085029@senacrs.edu.br>
2024-08-13 08:45:07 -0300

    animation

--- frontend/static/script_start.js
+++ frontend/static/script_start.js
@@ -1,3 +1,14 @@
 document.getElementById('startButton').addEventListener('click', function() {
     window.location.href = '/index.html';
 });
+
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    const mainContent = document.getElementById('how-it-works');
+
+    // Simular um carregamento (tempo de 3 segundos)
+    setTimeout(function() {
+        preloader.style.display = 'none';
+        mainContent.style.display = 'block';
+    }, 3000); // Tempo em milissegundos
+});
